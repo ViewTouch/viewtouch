a3d78e8ed6b484d61f5594d5395bf8b059247fa1
NeroBurner-tux <pyro4hell@gmail.com>
2018-07-06 07:03:21 +0200

    Modernize conf_file
    - use ofstream and ifstream
      use outputfilestream instead of more generic filesteam. WriteLn only
      outputs to a file. It would make no sense to read from a file in WriteLn
    - use constexpr instead of macro define
    - use default initializers for structs
    - use const references for most passed parameter
    - make GetKey and GetSection const (does not modify internal data
      structure)
    - use const references for passed parameter
    - fix memory leak: no need to create a object with new when passing it
      to std::vector
    - cerr understands std::string
    - cleanup Load()
      - for sections just look at the first character if it is '[', no need to
        search the whole string
      - don't store the current section, just the current section-name
    - update Save()
      - use << instead of WriteLn
    - update SetValue()
      - fix memory leak in SetValue
      - check key/value parameter for empty string
    - remove GetKey, remove iterator-type
    - remove function WriteLn, use ofstream directly
    - indentation for SetFilename
    - use std::string instead of t_Str

--- conf_file.hh
+++ conf_file.hh
@@ -18,46 +18,36 @@
 // Used simply as a max size of some internal buffers. Determines the maximum
 // length of a line that will be read from or written to the file or the
 // report output.
-#define MAX_BUFFER_LEN                1024
+constexpr size_t MAX_BUFFER_LEN = 1024;
 
-typedef std::string t_Str;
-const t_Str CommentIndicators = t_Str(";#");
-const t_Str EqualIndicators   = t_Str("=:"); 
-const t_Str WhiteSpace = t_Str(" \t\n\r");
+const std::string CommentIndicators = ";#";
+const std::string EqualIndicators   = "=:";
+const std::string WhiteSpace = " \t\n\r";
 
 // st_key
 // This structure stores the definition of a key. A key is a named identifier
 // that is associated with a value.
 typedef struct st_key
 {
-    t_Str        key;
-    t_Str        value;
+    std::string key   = "";
+    std::string value = "";
 
     st_key()
-    {
-        key = t_Str("");
-        value = t_Str("");
-    }
-
+    {}
 } t_Key;
 
 typedef std::vector<t_Key> KeyList;
-typedef KeyList::iterator KeyItor;
 
 // st_section
 // This structure stores the definition of a section. A section contains any number
 // of keys (see st_keys).
 typedef struct st_section
 {
-    t_Str        name;
-    KeyList      keys;
+    std::string  name = "";
+    KeyList      keys = {};
 
     st_section()
-    {
-        name = t_Str("");
-        keys.clear();
-    }
-
+    {}
 } t_Section;
 
 typedef std::vector<t_Section> SectionList;
@@ -67,59 +57,56 @@ typedef SectionList::iterator SectionItor;
 
 /// General Purpose Utility Functions ///////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-void  Trim(t_Str& szStr);
-int   WriteLn(std::fstream& stream, const char* fmt, ...);
+void  Trim(std::string& szStr);
 
 
 
 class ConfFile
 {
 public:
-                ConfFile(t_Str fileName, bool load = false);
+                ConfFile(const std::string &fileName, const bool load = false);
     virtual     ~ConfFile();
 
     bool        Load();
     bool        Save();
 
                 // get a key's value;  overloaded for type conversion convenience
-    bool        GetValue(t_Str &value, t_Str key, t_Str section = t_Str("")); 
-    bool        GetValue(Flt &value, t_Str key, t_Str section = t_Str(""));
-    bool        GetValue(int &value, t_Str key, t_Str section = t_Str(""));
+    bool        GetValue(std::string &value, const std::string &key, const std::string &section = "") const;
+    bool        GetValue(Flt &value, const std::string &key, const std::string &section = "") const;
+    bool        GetValue(int &value, const std::string &key, const std::string &section = "") const;
                 // set a key's value; created if doesn't exist
-    bool        SetValue(t_Str value, t_Str key, t_Str section = t_Str(""));
-    bool        SetValue(Flt value, t_Str key, t_Str section = t_Str(""));
-    bool        SetValue(int value, t_Str key, t_Str section = t_Str(""));
+    bool        SetValue(const std::string &value, const std::string &key, const std::string &section = "");
+    bool        SetValue(const Flt value, const std::string &key, const std::string &section = "");
+    bool        SetValue(const int value, const std::string &key, const std::string &section = "");
 
                 // remove a key from a section
-    bool        DeleteKey(t_Str key, t_Str section = t_Str(""));
+    bool        DeleteKey(const std::string &key, const std::string &section = "");
 
                 // remove a section from the file
-    bool        DeleteSection(t_Str section);
+    bool        DeleteSection(const std::string &section);
                 // CreateSection: Creates the new section if it does not allready
                 // exist. Section is created with no keys.
-    bool        CreateSection(t_Str section);
+    bool        CreateSection(const std::string &section);
 
                 // returns number of sections in file
-    int         SectionCount();
+    int         SectionCount() const;
                 // returns number of keys across all sections
-    int         KeyCount();
+    int         KeyCount() const;
 
-    void        SetFilename(t_Str name);
+    void        SetFilename(const std::string &name);
 
 
 protected:
-                // GetKey: Returns the requested key (if found) from the requested
-                // Section. Returns NULL otherwise.
-    t_Key*      GetKey(t_Str key, t_Str section);
-                // GetSection: Returns the requested section (if found), NULL otherwise.
-    t_Section*  GetSection(t_Str section);
+    // GetSection: Returns the requested section (if found), nullptr otherwise.
+    const t_Section* GetSection(const std::string &section) const;
+    t_Section* GetSection(const std::string &section);
 
 
 public:
 
 protected:
     SectionList sections; // Our list of sections
-    t_Str       fileName; // The filename to write to
+    std::string fileName; // The filename to write to
     bool        dirty;    // Tracks whether or not data has changed.
 };
 
