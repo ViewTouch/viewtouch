814d13912bf0d9b360740539f02b386b1fe219ed
NeroBurner-tux <pyro4hell@gmail.com>
2018-07-16 23:32:48 +0200

    main: addmission: don't include sha1.hh in header

--- main/admission.hh
+++ main/admission.hh
@@ -3,7 +3,6 @@
 
 #include "utility.hh"
 #include "sales.hh"
-#include <external/sha1.hh>
 
 void admission_itemname_hash(Str& ih,const Str& name,const Str& location,const Str& time, const Str& price_class); //converts a name to the item~hash form.
 void admission_parse_hash_name(Str& name,const Str& ih);
