cbd8d6d8702654b3f7842bbdde67a5185b427fb4
No0ne558 <arielbrambila117@gmail.com>
2025-07-22 10:36:33 -0700

    Fix date parsing compatibility with C++20 standard library

--- time_info.cc
+++ time_info.cc
@@ -102,19 +102,28 @@ int TimeInfo::Set(const std::string &date_string)
     using namespace date;
 
     std::istringstream ss(date_string);
-    sys_time<std::chrono::milliseconds> t;
-    ss >> date::parse("%d/%m/%y,%h:%m", t);
+    sys_time<std::chrono::seconds> t;
+    
+    // Try parsing with 2-digit year format first
+    ss >> date::parse("%d/%m/%y,%H:%M", t);
     if (ss.fail())
     {
-        ss >> date::parse("%d/%m/%Y,%h:%m", t);
+        // Reset stream and try 4-digit year format
+        ss.clear();
+        ss.str(date_string);
+        ss >> date::parse("%d/%m/%Y,%H:%M", t);
         if (ss.fail())
         {
             Clear();
-            return false;
+            return 1; // error
         }
     }
+    
+    // Convert to local time
+    auto zt = make_zoned(current_zone(), t);
+    t_ = zt.get_local_time();
     is_valid_ = true;
-    return true;
+    return 0; // success
 }
 
 int TimeInfo::Set(const TimeInfo *other)
