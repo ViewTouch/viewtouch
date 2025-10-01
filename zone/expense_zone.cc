3cd329b6eb0775f8442ac81a41f30cf4485332f5
No0ne558 <arielbrambila117@gmail.com>
2025-09-30 17:39:31 -0700

    Update copyright headers to include 2025
    Updated all 146 ViewTouch source files to include 2025 in copyright headers.
    
    Changes:
    - Updated copyright year from '1995, 1996, 1997, 1998' to include ', 2025'
    - Updated copyright year from '1995, 1996, 1997' to include ', 2025'
    - Maintains full copyright history while acknowledging current year
    - Applied consistently across all .cc, .hh, and .h files
    
    Files modified: 146
    Build status: ✅ Verified - all targets compile successfully

--- zone/expense_zone.cc
+++ zone/expense_zone.cc
@@ -1,5 +1,5 @@
  /*
- * Copyright ViewTouch, Inc., 1995, 1996, 1997, 1998  
+ * Copyright ViewTouch, Inc., 1995, 1996, 1997, 1998, 2025
   
  *   This program is free software: you can redistribute it and/or modify 
  *   it under the terms of the GNU General Public License as published by 
--- zone/user_edit_zone.hh
+++ zone/user_edit_zone.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright ViewTouch, Inc., 1995, 1996, 1997  
+ * Copyright ViewTouch, Inc., 1995, 1996, 1997, 2025
   
  *   This program is free software: you can redistribute it and/or modify 
  *   it under the terms of the GNU General Public License as published by 
